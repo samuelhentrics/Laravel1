6fd26911d4b43c417b594b9bf2aafeb699a22be5
Samuel HENTRICS <samuel.hentrics@gmail.com>
2023-03-23 11:17:54 +0100

    Formulaire d'inscription/logijn

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'prenom' => fake()->name(),
+            'age' => fake()->numberBetween(10, 99),
+            'adresse' => fake()->address(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
